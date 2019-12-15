05e5734ae2ab1e4ab30f1482056b79c9fffac516
fredeeb <frederikbraendstrup@gmail.com>
2019-12-15 21:50:35 +0100

    removed C-p to avoid any more disasters

--- .zsh/keybinds.zsh
+++ .zsh/keybinds.zsh
@@ -1,20 +1,3 @@
-#git push
-function git_prepare() {
-    if [ -n "$BUFFER" ];
-    then
-	BUFFER="git add -A && git commit -m \"$BUFFER\" && git push"
-    fi
-
-    if [ -z "$BUFFER" ];
-    then
-	BUFFER="git add -A && git commit -v && git push"
-    fi
-
-    zle accept-line
-}
-zle -N git_prepare
-bindkey "^p" git_prepare
-
 # fzf
 # Key bindings
 # ------------
