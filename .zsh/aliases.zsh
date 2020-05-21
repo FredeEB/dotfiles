bf362074441430d4340eae1a0179c9e312e619d0
fredeb <im@fredeb.dev>
2020-05-21 10:00:06 +0200

    updated some aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,7 +1,7 @@
 #devtools
 alias e="emacs . &"
 alias ec="emacsclient -c . & disown"
-alias et="emacsclient -t ."
+alias et="emacsclient -t"
 alias gs="git status"
 alias ne="nix-shell --command 'emacs . &'"
 alias remacs="systemctl restart --user emacs"
@@ -17,8 +17,8 @@ alias la="ls -lah"
 alias lr="ls -lahR | sed -e '/^total.*$/d' -e '/.*\\.$/d'"
 alias ls='ls --color=auto'
 alias mv="mv -i"
-alias r="ranger ."
 alias rf="rm -rf"
+alias r='ranger --choosedir=$HOME/.rangerdir; LASTDIR=`cat $HOME/.rangerdir`; cd "$LASTDIR"'
 alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
@@ -26,12 +26,8 @@ alias .....="cd ../../../.."
 
 #git
 alias gd="git diff"
-alias gl="git log --graph"
+alias gl="git log --graph --oneline --all"
 alias gp="git pull"
-
-#nix
-alias ns="nix-shell"
-alias nse="nix-shell --command 'emacs . &'"
 
 #docker
 alias dps="docker ps -a"
@@ -40,18 +36,10 @@ alias dcu="docker-compose up"
 alias dcd="docker-compose up -d"
 alias dck="docker-compose down"
 
-#apt
-alias inst="sudo apt install -y"
-alias remo="sudo apt autoremove -y"
-alias upgr="sudo apt update && sudo apt upgrade -y"
-
 #misc
 alias c="clear"
 alias chat="ssh bun@chat.shazow.net"
 alias s="shutdown +60"
-alias sisu=". /opt/poky/2.6/environment-setup-arm1176jzfshf-vfp-poky-linux-gnueabi"
 alias z="zathura"
-alias rec="ffmpeg -f x11grab -s `xdpyinfo | grep dimensions | sed -r "s/.*([0-9]{4}x[0-9]{4}).*/\1/g"
-` -i :0.0 -f alsa -i default"
-
+alias rec="ffmpeg -f x11grab -s `xdpyinfo | grep dimensions | sed -r "s/.*([0-9]{4}x[0-9]{4}).*/\1/g"` -i :0.0 -f alsa -i default"
 #unsorted
