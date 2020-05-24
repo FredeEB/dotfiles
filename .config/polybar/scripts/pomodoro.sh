56e061b45860fc43bef76e40a2bdb45a54188161
fredeb <im@fredeb.dev>
2020-05-24 18:01:31 +0200

    added restart capabilities to pomodoro timer

--- .config/polybar/scripts/pomodoro.sh
+++ .config/polybar/scripts/pomodoro.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-notify-send Work!
+notify-send -n 4 Work!
 for i in {25..1}; do echo Work: $i min; sleep 60; done;
 
-notify-send Break!
+notify-send -n 4 Break!
 for i in {5..1}; do echo Break: $i min; sleep 60; done;
 
 echo "Done!"
+while true; do sleep 3600
+done
