9d3269c0fcd5185bf50c0d929c944b0e9d2f316b
Frederik Braendstrup <fbraendstrup@roku.com>
2021-11-01 08:55:43 +0100

    updated gl alias

--- bash/.config/bash/aliases.bash
+++ bash/.config/bash/aliases.bash
@@ -19,7 +19,8 @@ alias ls="exa"
 #git
 alias gs="git status"
 alias gd="git diff"
-alias gl="git log --graph --oneline --all"
+alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+alias gla="gl --all"
 alias gp="git pull"
 
 #docker
