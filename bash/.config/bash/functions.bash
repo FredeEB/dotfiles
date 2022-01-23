17a09390e3f2d5676fadf1b302723f00d83bfd49
Frede Braendstrup <frederikbraendstrup@gmail.com>
2022-01-23 23:02:44 +0100

    fixed rebases in fzf git commands

--- bash/.config/bash/functions.bash
+++ bash/.config/bash/functions.bash
@@ -14,7 +14,7 @@ function gl() {
             --bind "ctrl-u:preview-page-up" \
             --bind "ctrl-s:execute(git show {1})" \
             --bind "ctrl-o:execute(git checkout {1})+accept" \
-            --bind "ctrl-r:execute(git rebase -i {1})+accept" \
+            --bind "ctrl-r:execute(git rebase -i {1}^)+accept" \
             --bind "ctrl-m:execute(git show {1})+accept"
 }; export -f gl
 
@@ -22,6 +22,7 @@ function gbr() {
     git --no-pager branch \
         | fzf --reverse --preview="git --no-pager log --oneline --color=always --graph {-1}" \
             --bind "enter:execute(git checkout {-1})+accept" \
+            --bind "ctrl-r:execute(git rebase -i {1}^)+accept" \
             --bind "ctrl-s:execute(gl {-1})+accept"
 }
 
