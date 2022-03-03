125fe094ae1c5dcb1c2631ac55b99a120fad1eeb
Frede Braendstrup <frederikbraendstrup@gmail.com>
2022-03-04 00:41:04 +0100

    changed <leader>q to do :q

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -32,7 +32,7 @@ m.keys{ -- vblock moves
 }
 
 m.keys{ -- closing files
-    {'n', '<leader>q', [[<cmd>bdelete<cr>]]},
+    {'n', '<leader>q', [[<cmd>q<cr>]]},
     {'n', '<leader>Q', [[<cmd>q!<cr>]]},
 }
 
