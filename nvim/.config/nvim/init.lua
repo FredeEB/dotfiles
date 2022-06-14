acb920d0e61443f3ab66ba8816a015ff4798275a
Frederik Braendstrup <frederikbraendstrup@gmail.com>
2022-06-14 09:02:51 +0200

    enable current_line_blame

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -513,7 +513,7 @@ m.key('n', 'U', [[<cmd>UndotreeToggle<cr>]])
 
 -- git
 require('gitsigns').setup({
-    current_line_blame = false,
+    current_line_blame = true,
     current_line_blame_opts = {
         delay = 500,
     }
