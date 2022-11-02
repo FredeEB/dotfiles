9cfc1e57d369b491a4dd4633fb55fac5ec23957b
Frede Braendstrup <frederikbraendstrup@gmail.com>
2022-11-02 19:38:16 +0100

    fixed packer bootstrap

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -113,7 +113,8 @@ m.keys { -- misc
 local packer_bootstrap = false
 local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-    packer_bootstrap = vim.fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+    vim.fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+    packer_bootstrap = true
 end
 
 local packer = require('packer')
