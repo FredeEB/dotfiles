a1ff34635c294217690f5bda4a4412280f061215
Frederik Braendstrup <frederikbraendstrup@gmail.com>
2023-01-13 10:54:15 +0100

    added more keybinds to git-fzf functions

--- bash/.config/bash/functions.bash
+++ bash/.config/bash/functions.bash
@@ -9,27 +9,31 @@ alias gd="git diff"
 function gl() {
     COMMITISH=($@); shift
     git --no-pager log --oneline ${COMMITISH[@]} \
-        | fzf --no-sort --reverse --preview "git --no-pager show --ext-diff {1}" \
+        | fzf -m --no-sort --reverse --preview "git --no-pager show --ext-diff {1}" \
             --bind "ctrl-d:preview-page-down" \
             --bind "ctrl-u:preview-page-up" \
-            --bind "ctrl-s:execute(git show {1})" \
             --bind "ctrl-o:execute(git checkout {1})+accept" \
             --bind "ctrl-r:execute(git rebase -i {1}^)+accept" \
-            --bind "ctrl-m:execute(git show {1})+accept"
+            --bind "ctrl-a:execute-multi(git cherry-pick {1})+accept" \
+            --bind "ctrl-s:execute(git show {1})+accept"
 }; export -f gl
 
 function gbr() {
     git --no-pager branch \
         | fzf --reverse --preview="git --no-pager log --oneline --color=always --graph {-1}" \
+            --bind "ctrl-d:preview-page-down" \
+            --bind "ctrl-u:preview-page-up" \
             --bind "enter:execute(git checkout {-1})+accept" \
             --bind "ctrl-r:execute(git rebase -i {-1})+accept" \
-            --bind "ctrl-d:execute(git branch -d {-1})" \
+            --bind "ctrl-x:execute(git branch -d {-1})" \
             --bind "ctrl-s:execute(gl {-1})+accept"
 }
 
 function gba() {
     git --no-pager reflog\
         | fzf --no-sort --reverse --preview="git --no-pager show --ext-diff {1}" \
+            --bind "ctrl-d:preview-page-down" \
+            --bind "ctrl-u:preview-page-up" \
             --bind "enter:execute(git checkout {1})+accept" \
             --bind "ctrl-s:execute(gl {1})+accept" 
 }
