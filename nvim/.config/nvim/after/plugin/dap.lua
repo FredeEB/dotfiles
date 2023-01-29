9be9c82c366b54d46bbf67d6ac2f07b66d5059b7
Frede Braendstrup <frederikbraendstrup@gmail.com>
2023-01-29 20:49:37 +0100

    fixed dap for C++

--- nvim/.config/nvim/after/plugin/dap.lua
+++ nvim/.config/nvim/after/plugin/dap.lua
@@ -1,27 +1,53 @@
 local hydra = require('hydra')
 -- dap
 local dap = require('dap')
+
+dap.adapters.cppdbg = {
+    id = 'cppdbg',
+    type = 'executable',
+    command = 'OpenDebugAD7',
+}
 dap.configurations.cpp = {
     {
-        name = 'Launch',
-        type = 'lldb',
+        name = 'Attach to gdbserver',
+        type = 'cppdbg',
         request = 'launch',
+        MIMode = 'gdb',
+        miDebuggerServerAddress = 'localhost:5555',
+        miDebuggerPath = 'gdb',
+        cwd = '${workspaceFolder}',
         program = function()
             return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
         end,
-        cwd = "${workspaceFolder}",
-        stopOneEntry = false,
-        args = function()
-            local args = vim.fn.input('Args: ')
-            return vim.fn.split(args, ' ')
+    },
+    {
+        name = 'Debug program',
+        type = 'cppdbg',
+        request = 'launch',
+        MIMode = 'gdb',
+        miDebuggerPath = 'gdb',
+        cwd = '${workspaceFolder}',
+        program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
         end,
     }
 }
 
+require('mason-nvim-dap').setup {
+    ensure_installed = { "cppdbg" },
+    automatic_setup = true,
+}
+
 local dapui = require("dapui")
-dap.listeners.after.event_initialized["dapui_config"] = dapui.open
-dap.listeners.before.event_terminated["dapui_config"] = dapui.close
-dap.listeners.before.event_exited["dapui_config"] = dapui.close
+dap.listeners.after.event_initialized["dapui_config"] = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+  dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+  dapui.close()
+end
 
 hydra({
     name = 'Dap Debug',
@@ -33,11 +59,10 @@ hydra({
     mode = { 'n', 'x' },
     body = '<leader>d',
     heads = {
-        { 'c', require('dap').continues, { desc = 'continue' } },
+        { 'c', require('dap').continue, { desc = 'continue' } },
         { 'b', require('dap').toggle_breakpoint, { desc = 'toggle breakpoint' } },
         { 's', require('dap').step_over, { desc = 'step over' } },
         { 'i', require('dap').step_into, { desc = 'step into' } },
         { 'q', require('dap').close, { desc = 'stop' } },
     }
 })
-
