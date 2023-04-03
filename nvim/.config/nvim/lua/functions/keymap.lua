56038d8b4d26b92201fe264cf6ae56d9d97657b9
Frederik Braendstrup <frederikbraendstrup@gmail.com>
2023-04-03 09:56:13 +0200

    add support for options in keys_for_filetype

--- nvim/.config/nvim/lua/functions/keymap.lua
+++ nvim/.config/nvim/lua/functions/keymap.lua
@@ -15,20 +15,20 @@ end
 
 -- map keys for filetypes
 vim.api.nvim_create_augroup('KeybindsForMode', { clear = true })
-function map.key_for_filetype(filetype, mode, mapping, cmd)
+function map.key_for_filetype(filetype, mode, mapping, cmd, options)
     vim.api.nvim_create_autocmd('FileType', {
         group = 'KeybindsForMode',
         pattern = filetype,
         callback = function()
-            map.key(mode, mapping, cmd)
+            map.key(mode, mapping, cmd, options)
         end
     })
 end
 -- map many keys at once for filetype
 -- format is {{ filetype, mode, mapping, cmd }, ...}
-function map.keys_for_filetype(maps)
+function map.keys_for_filetype(maps, options)
     for v = 1, #maps do
-        map.key_for_filetype(maps[v][1], maps[v][2], maps[v][3], maps[v][4])
+        map.key_for_filetype(maps[v][1], maps[v][2], maps[v][3], maps[v][4], options)
     end
 end
 
