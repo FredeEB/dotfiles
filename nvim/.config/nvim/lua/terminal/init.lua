6ac2093127237e145fda44eefb993ca6334a5797
Frederik Braendstrup <fbraendstrup@IT-03339.tailfa7ea.ts.net>
2023-07-19 11:10:44 +0200

    use tmux in terminal plugin

--- nvim/.config/nvim/lua/terminal/init.lua
+++ nvim/.config/nvim/lua/terminal/init.lua
@@ -1,19 +1,8 @@
 local default_terminal_options = {
     replace = false,
     vertical = false,
-    shell = 'screen',
-    shell_params = {},
-    session = nil,
+    keep_dir = true,
 }
-
-local function join(list, ch)
-    ch = ch or ' '
-    local res = ''
-    for _, item in pairs(list) do
-        res = res .. item .. ch
-    end
-    return res
-end
 
 local function get_opts(opts)
     if opts == nil then
@@ -29,44 +18,19 @@ local function get_opts(opts)
     return res
 end
 
-local get_shell_command = function(opts)
-    local vim_cmd = 'split'
-    if opts.replace then
-        vim_cmd = 'e'
-    elseif opts.vertical then
-        vim_cmd = 'vsplit'
-    end
-    return vim_cmd .. ' term://' .. opts.shell .. ' ' .. join(opts.shell_params)
-end
-
-local attach_to_screen_session = function(session, opts)
-    local cmd =
-        vim.cmd(get_shell_command(opts) .. session)
-end
-
-local get_screen_sessions = function()
-    local res = {}
-    for session in vim.fs.dir('/run/screens/S-' .. os.getenv('USER')) do
-        local program = 'bash'
-        table.insert(res, {
-            session = session,
-            program = program,
-        })
-    end
-    return res
-end
-
 local open_terminal = function(opts)
     opts = get_opts(opts)
-    if opts.session == nil then
-        local c = get_shell_command(opts)
-        vim.cmd(c)
+    local args = opts.keep_dir and " -c %:p:h" or ""
+    vim.cmd('!export NVIM=$NVIM')
+    if opts.replace then
+        vim.cmd('!tmux neww' .. args)
     else
-        attach_to_screen_session(sessions[1], opts)
+        local direction = opts.vertical and ' -v' or ' -h'
+        vim.cmd('!tmux split' .. direction .. args)
     end
+    vim.cmd('redraw!')
 end
 
 return {
     open_terminal = open_terminal,
-    get_screen_sessions = get_screen_sessions,
 }
