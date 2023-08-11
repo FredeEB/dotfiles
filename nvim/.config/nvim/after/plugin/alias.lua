4768ac5361716872975071a85cfb964cf3b4be19
Frederik Braendstrup <frederikbraendstrup@gmail.com>
2023-08-11 09:48:10 +0200

    fixed aliases

--- nvim/.config/nvim/after/plugin/alias.lua
+++ nvim/.config/nvim/after/plugin/alias.lua
@@ -4,9 +4,10 @@ require('alias-nvim').setup {
         m = 'Man $1',
     },
     functions = {
-        gs = function (cwd) require('neogit').open { cwd = cwd } end,
-        gd = function (cwd) require('diffview').open { cwd = cwd } end,
-        gl = function () require('fzf-lua').git_commits() end,
-        gb = function () require('fzf-lua').git_branches() end,
+        gs = function(opts) require('neogit').open { cwd = opts.cwd } end,
+        gd = function(opts) require('diffview').open { cwd = opts.cwd } end,
+        gl = function(opts) require('fzf-lua').git_commits { cwd = opts.cwd } end,
+        gb = function(opts) require('fzf-lua').git_branches { cwd = opts.cwd } end,
+
     }
 }
