717dcce82c4b8eec80c23c6be920f1d2d3aadd6f
Frede Braendstrup <frederikbraendstrup@gmail.com>
2023-08-22 13:46:56 +0200

    fix tmux integration

--- nvim/.config/nvim/after/plugin/tmux.lua
+++ nvim/.config/nvim/after/plugin/tmux.lua
@@ -1,22 +1,23 @@
 local m = require('functions.keymap')
 local term = require('terminal')
 
+local function set_nvim_env_var()
+    local nvim_sock = os.getenv('NVIM')
+    if nvim_sock ~= nil and os.rename(nvim_sock, nvim_sock) then return end
+    if os.getenv('TMUX') then
+        vim.fn.system([[tmux setenv NVIM ]] .. vim.v.servername)
+    end
+end
+
+local function unset_nvim_env_var()
+    if os.getenv('NVIM') == vim.v.servername then
+        vim.fn.system([[tmux setenv -u NVIM]])
+    end
+end
+
 vim.api.nvim_create_augroup('TmuxManip', { clear = true })
-vim.api.nvim_create_autocmd('VimEnter', {
-    callback = function ()
-        local nvim_sock = os.getenv('NVIM')
-        if os.getenv('TMUX') and nvim_sock ~= nil and not os.rename(nvim_sock, nvim_sock) then
-            vim.fn.system([[tmux setenv NVIM ]] .. vim.v.servername)
-        end
-    end
-})
-vim.api.nvim_create_autocmd('VimLeave', {
-    callback = function ()
-        if os.getenv('NVIM') == vim.v.servername then
-            vim.fn.system([[tmux setenv -u NVIM]])
-        end
-    end
-})
+vim.api.nvim_create_autocmd('VimEnter', { callback = set_nvim_env_var })
+vim.api.nvim_create_autocmd('VimLeave', { callback = unset_nvim_env_var})
 
 m.keys {
     -- Open new terminals
