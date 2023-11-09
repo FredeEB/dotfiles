fecc1f152a33657413a328683d1fd0a01ced3204
Frede Braendstrup <frederikbraendstrup@gmail.com>
2023-11-09 20:33:12 +0100

    disable ligatures

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -9,6 +9,7 @@ end
 
 config.color_scheme = 'Tokyo Night'
 config.font = wezterm.font('Iosevka Nerd Font')
+config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 config.enable_tab_bar = false
 
