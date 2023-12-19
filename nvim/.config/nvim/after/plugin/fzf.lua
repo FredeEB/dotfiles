dd8d97de642b34612e3eb8202136b52284c58bd8
Frede Braendstrup <frederikbraendstrup@gmail.com>
2023-12-19 11:43:55 +0100

    nvim: sensible rg with fzflua

--- nvim/.config/nvim/after/plugin/fzf.lua
+++ nvim/.config/nvim/after/plugin/fzf.lua
@@ -13,6 +13,9 @@ fzf.setup {
             default = 'bat'
         }
     },
+    grep = {
+        rg_opts = "--column --line-number --no-heading --color=always --smart-case --max-columns=4096 --hidden -g '!.git' -e",
+    },
     previewers = {
         bat = {
             theme = 'Dracula'
