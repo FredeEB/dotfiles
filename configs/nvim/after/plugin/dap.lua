49c78c0867d4dc744bcd8ad04e8dca0a73b8d792
Frede Braendstrup <frederikbraendstrup@gmail.com>
2024-05-20 15:59:38 +0200

    nvim: wrap dap eventhandlers in functions

--- configs/nvim/after/plugin/dap.lua
+++ configs/nvim/after/plugin/dap.lua
@@ -76,10 +76,19 @@ dap.configurations.lua = {
     },
 }
 
-local dapui = require("dapui")
-dap.listeners.after.event_initialized["dapui_config"] = dapui.open
-dap.listeners.before.event_terminated["dapui_config"] = dapui.close
-dap.listeners.before.event_exited["dapui_config"] = dapui.close
+local dapui = require('dapui')
+dap.listeners.before.attach.dapui_config = function()
+    dapui.open()
+end
+dap.listeners.before.launch.dapui_config = function()
+    dapui.open()
+end
+dap.listeners.before.event_terminated.dapui_config = function()
+    dapui.close()
+end
+dap.listeners.before.event_exited.dapui_config = function()
+    dapui.close()
+end
 
 local Hydra = require('hydra')
 local widgets = require('dap.ui.widgets')
