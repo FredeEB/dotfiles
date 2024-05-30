8444e9ef88c983532a88ad4fb95ac1ec408cf8b9
Frede Braendstrup <frederikbraendstrup@gmail.com>
2024-05-31 01:22:15 +0200

    nix: add resolved

--- common/base.nix
+++ common/base.nix
@@ -18,6 +18,13 @@
 
   # Enable networking
   networking.nameservers = [ "1.1.1.1" "1.0.0.1" ];
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+    domains = [ "~." ];
+    fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+    dnsovertls = "true";
+  };
   # Set your time zone.
   time.timeZone = "Europe/Copenhagen";
 
