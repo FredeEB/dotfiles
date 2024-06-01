362ff2698f3f2d4cee64c52d4e539029c8e65967
Frede Braendstrup <frederikbraendstrup@gmail.com>
2024-06-01 20:28:43 +0200

    nix: add homelab config

---
+++ systems/homelab.nix
@@ -0,0 +1,17 @@
+{ config, lib, modulesPath, ... }: {
+  imports = [
+    ../modules/default-system-layout.nix
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
+
+  boot.initrd.availableKernelModules = [ "ahci" "ohci_pci" "ehci_pci" "pata_atiixp" "xhci_pci" "firewire_ohci" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+
+  networking.hostName = "server";
+  networking.useDHCP = lib.mkDefault true;
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode =
+    lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
