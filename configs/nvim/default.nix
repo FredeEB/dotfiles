0bf5a8108e71ad011444b8cb9a9e83ce38e91cf3
Frede Braendstrup <frederikbraendstrup@gmail.com>
2024-06-17 20:46:19 +0200

    fix: install nvim config with nix module

--- configs/nvim/default.nix
+++ configs/nvim/default.nix
@@ -6,10 +6,10 @@ stdenv.mkDerivation {
   installPhase = ''
     runHook preInstall
 
-    mkdir -p $out/share/nvim/{lua/functions,after/plugin}
-    install *.lua $out/share/nvim/
-    install lua/functions/*.lua $out/share/nvim/lua/functions
-    install after/plugin/*lua $out/share/nvim/after/plugin
+    mkdir -p $out/share/nvim/config/{lua/functions,after/plugin}
+    install *.lua $out/share/nvim/config/
+    install lua/functions/*.lua $out/share/nvim/config/lua/functions
+    install after/plugin/*lua $out/share/nvim/config/after/plugin
 
     runHook postInstall
   '';
